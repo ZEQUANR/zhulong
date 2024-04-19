118d58e1a9b1112fa3d071ce988ab54b8aba1dc2
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-04-19 17:14:16 +0800

    Feature: gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-
-	"github.com/ZEQUANR/zhulong/driver"
-	"github.com/ZEQUANR/zhulong/ent"
+	"github.com/ZEQUANR/zhulong/routers"
+	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-	u, err := client.User.
-		Create().
-		SetAge(30).
-		SetName("a8m").
-		Save(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating user: %w", err)
-	}
-	log.Println("user was created: ", u)
-	return u, nil
-}
-
 func main() {
-	CreateUser(context.Background(), driver.MysqlClient)
+	r := gin.Default()
+
+	routers.Init(r)
+
+	r.Run()
 }
